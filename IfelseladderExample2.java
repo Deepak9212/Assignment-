b48bf84a0605811a58ebae7c8892b2232e0dc398
Coding with Deepak <113295737+Deepak9212@users.noreply.github.com>
2023-01-24 17:59:51 +0530

    Add files via upload

---
+++ IfelseladderExample2.java
@@ -0,0 +1,16 @@
+class IfelseladderExample2{
+public static void main(String[]args){
+int x=50;
+
+if (x>0){
+System.out.println("number is positive");
+}
+
+else if (x<0){ 
+System.out.println("number is negative ");
+}
+else{ 
+System.out.println("zero");
+}
+}
+}
