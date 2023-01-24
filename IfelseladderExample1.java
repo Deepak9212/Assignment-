df3099927da098f08ad68da38034e028298da41a
Coding with Deepak <113295737+Deepak9212@users.noreply.github.com>
2023-01-24 17:51:04 +0530

    Add files via upload

---
+++ IfelseladderExample1.java
@@ -0,0 +1,23 @@
+class IfelseladderExample1{
+public static void main(String[]args){
+int x=40;
+
+if (x==10){
+System.out.println("Value of x is 10");
+}
+
+else if (x==20){ 
+System.out.println("Value of x is 20");
+}
+
+else if (x==30){
+System.out.println("Value of x is 30");
+}
+
+else if(x==40){
+System.out.println("Value of x is 40");
+}else{
+System.out.println("This is else statement");
+}
+}
+}
