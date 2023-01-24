e66306585d46abdc331ab983382f017687370ae6
Coding with Deepak <113295737+Deepak9212@users.noreply.github.com>
2023-01-24 16:48:20 +0530

    Add files via upload

---
+++ IfElseExample2.java
@@ -0,0 +1,18 @@
+
+
+class IfElseExample2{
+
+public static void main (String[]args){
+
+int number=30;
+if (number>0){
+
+System.out.println("the number is positive");
+}
+else {
+System.out.println("the number is negative");
+}
+}
+}
+
+
---
+++ IfElseExample3.java
@@ -0,0 +1,18 @@
+
+
+class IfElseExample3{
+
+public static void main (String[]args){
+
+int age=21;
+if (age>18){
+
+System.out.println("You are eligible for voting");
+}
+else {
+System.out.println("You are not eligible for voting");
+}
+}
+}
+
+
