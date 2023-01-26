f151ecfb408913cb367f0d3f1f6fd821fa13329c
Coding with Deepak <113295737+Deepak9212@users.noreply.github.com>
2023-01-26 18:16:53 +0530

    Add files via upload

---
+++ NestedIfExample3.java
@@ -0,0 +1,12 @@
+class NestedIfExample3{
+public static void main(String[]args){
+
+//creating two variables age and weight
+int age=22;
+int weight=60;
+if(age>=18);{
+if(weight>40){
+System.out.println("you are eligible to donate blood");
+}
+}
+}}
---
+++ NestedIfExample4.java
@@ -0,0 +1,21 @@
+class NestedIfExample5{      
+public static void main(String[] args) {      
+    //Creating three variables x,y and z.   
+    int int x=30,y=40,z=20;    
+    if(x==30){      
+   
+    if(y>=50){      
+            
+            System.out.println("ABC");    
+        } if{
+if(z>30){  
+            System.out.println("DEF");    
+       
+     else 
+      System.out.println("PQR");  
+    } 
+else{
+System.out.println("XYZ"); 
+}  
+}
+}  
